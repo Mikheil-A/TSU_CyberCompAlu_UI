e4826e1f77169a5e7c805dc8a4d076526e18751a
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-04-10 17:04:39 +0400

    created header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthenticationComponent} from "./components/authentication/authentication.component";
+import {SignInComponent} from "./components/authentication/sign-in/sign-in.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'auth', component: AuthenticationComponent,
+    children: [
+      {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
+      {path: 'sign-in', component: SignInComponent}
+    ]
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/shared/header/header.component';
+import {AuthenticationComponent} from './components/authentication/authentication.component';
+import {SignInComponent} from './components/authentication/sign-in/sign-in.component';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthenticationComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
